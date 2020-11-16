9c7919989e341a3c935b54bcf6dee5e92fb3545e
Roxane Momeni <roxane.momeni@gmail.com>
2020-11-16 11:48:17 -0500

    edits to try_me function

--- tests/lib_test.py
+++ tests/lib_test.py
@@ -1,12 +1,5 @@
 # -*- coding: UTF-8 -*-
 
 # Import from standard library
-import os
-import happymonday
-import pandas as pd
-# Import from our lib
-from happymonday.lib import clean_data
-import pytest
-
 
 
